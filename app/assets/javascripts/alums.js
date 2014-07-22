15f7888c89df716d95645efaf71be2ec96392d80
Jeff Conway <jconway@arcwebtech.com>
2014-07-21 23:32:26 -0400

    Fix stupid JS crap

--- app/assets/javascripts/alums.js
+++ app/assets/javascripts/alums.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
+$(function(){
+  initPage();
+});
+$(window).bind('page:change', function(){
+  initPage();
+});
+
+function initPage(){
   $("#alumni-table").tablesorter({
     sortList: [[1, 0]],
     widgets: ["zebra","filter"]
   });
-});
+}
